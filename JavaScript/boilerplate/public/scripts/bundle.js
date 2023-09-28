94ff3119ef03fb283df020df23dbdd083ffc2e01
NehalSoni765 <nebakso16@gmail.com>
2023-09-28 22:30:07 +0530

    convert todo app into boiler plate

--- JavaScript/boilerplate/public/scripts/bundle.js
+++ JavaScript/boilerplate/public/scripts/bundle.js
@@ -82,6 +82,14 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/utils.js");
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
@@ -90,8 +98,8 @@ function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _ty
 function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 
 console.log("ADD " + (0,_utils__WEBPACK_IMPORTED_MODULE_0__.add)(2, 3));
-var name = 'Jan';
-console.log(name);
+var authorName = "Jan";
+console.log(authorName);
 var Hangman = /*#__PURE__*/function () {
   function Hangman() {
     _classCallCheck(this, Hangman);
@@ -99,13 +107,77 @@ var Hangman = /*#__PURE__*/function () {
   _createClass(Hangman, [{
     key: "method",
     value: function method() {
-      return 'Testing';
+      return "Testing";
     }
   }]);
   return Hangman;
 }();
 var hangman = new Hangman();
 console.log(hangman.method());
+
+// //calculate avg using class component
+// const calculateAvg = (...numbers)=>{
+//     let sum = 0;
+//     numbers.forEach(num=>sum+=num);
+//     const avg = sum/numbers.length;
+//     return `The avg grade is ${avg}`
+// }
+// console.log(calculateAvg(6,70,80,50,90));
+
+var calculateAvg = function calculateAvg(thing) {
+  var sum = 0;
+  for (var _len = arguments.length, numbers = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    numbers[_key - 1] = arguments[_key];
+  }
+  numbers.forEach(function (num) {
+    return sum += num;
+  });
+  var avg = sum / numbers.length;
+  return "The avg ".concat(thing, " is ").concat(avg);
+};
+console.log(calculateAvg("grade", 6, 70, 80, 50, 90));
+var createTeam = function createTeam(teamName, coach) {
+  console.log("Teams : ", teamName);
+  console.log("Coach : ", coach);
+  for (var _len2 = arguments.length, players = new Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
+    players[_key2 - 2] = arguments[_key2];
+  }
+  console.log("Teams : ".concat(players.join(", ")));
+};
+console.log(createTeam("Liberty", "Casey Penn", "Marge", "Aiden", "Herbert", "Sherry"));
+var team = {
+  name: "Liberty",
+  coach: "Casey Penn",
+  players: ["Marge", "Aiden", "Herbert", "Sherry"],
+  details: "grocerry"
+};
+console.log(team.players);
+createTeam.apply(void 0, [team.name, team.coach].concat(_toConsumableArray(team.players)));
+
+//destructuring {name:teamName is assign value to teamName variable}
+//assign destructureing variable is default value
+var formName = team.name,
+  coach = team.coach,
+  players = team.players,
+  _team$details = team.details,
+  otherDetails = _team$details === void 0 ? "No details provided" : _team$details,
+  other = _objectWithoutProperties(team, ["name", "coach", "players", "details"]);
+console.log("details ", formName);
+console.log("otherDetails ", otherDetails);
+console.log("other ", other);
+var age = [33, 25, 4, 35];
+// const [firstage,secondage,,lastage] = age
+// console.log("lastage ",lastage);
+
+var firstage = age[0],
+  otherAges = age.slice(1);
+console.log("otherAges ", otherAges);
+var printTeam = function printTeam(_ref) {
+  var name = _ref.name,
+    coach = _ref.coach;
+  console.log("".concat(name, ": ").concat(coach));
+};
+printTeam(team);
 })();
 
 /******/ })()
--- JavaScript/boilerplate/src/index.js
+++ JavaScript/boilerplate/src/index.js
@@ -1,14 +1,69 @@
 import { add } from "./utils";
-console.log("ADD "+add(2,3));
+console.log("ADD " + add(2, 3));
 
-const name='Jan'
-console.log(name);
+const authorName = "Jan";
+console.log(authorName);
 
-class Hangman{
-    method(){
-        return 'Testing'
-    }
+class Hangman {
+  method() {
+    return "Testing";
+  }
 }
 
 const hangman = new Hangman();
 console.log(hangman.method());
+
+// //calculate avg using class component
+// const calculateAvg = (...numbers)=>{
+//     let sum = 0;
+//     numbers.forEach(num=>sum+=num);
+//     const avg = sum/numbers.length;
+//     return `The avg grade is ${avg}`
+// }
+// console.log(calculateAvg(6,70,80,50,90));
+
+const calculateAvg = (thing, ...numbers) => {
+  let sum = 0;
+  numbers.forEach((num) => (sum += num));
+  const avg = sum / numbers.length;
+  return `The avg ${thing} is ${avg}`;
+};
+console.log(calculateAvg("grade", 6, 70, 80, 50, 90));
+
+const createTeam = (teamName, coach, ...players) => {
+  console.log("Teams : ", teamName);
+  console.log("Coach : ", coach);
+  console.log(`Teams : ${players.join(", ")}`);
+};
+console.log(
+  createTeam("Liberty", "Casey Penn", "Marge", "Aiden", "Herbert", "Sherry")
+);
+
+const team = {
+  name: "Liberty",
+  coach: "Casey Penn",
+  players: ["Marge", "Aiden", "Herbert", "Sherry"],
+  details:"grocerry"
+};
+console.log(team.players);
+createTeam(team.name, team.coach, ...team.players);
+
+//destructuring {name:teamName is assign value to teamName variable}
+//assign destructureing variable is default value
+const { name:formName, coach, players,details:otherDetails="No details provided",...other } = team;
+console.log("details ", formName);
+console.log("otherDetails ", otherDetails);
+console.log("other ", other);
+
+
+const age = [33,25,4,35]
+// const [firstage,secondage,,lastage] = age
+// console.log("lastage ",lastage);
+
+const [firstage,...otherAges] = age
+console.log("otherAges ",otherAges);
+
+const printTeam =({name,coach})=>{
+    console.log(`${name}: ${coach}`);
+}
+printTeam(team);
